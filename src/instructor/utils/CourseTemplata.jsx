07273c84d0046f7e1ac1083ab4e6452e222268d2
nithish0302 <nithi5palani@gmail.com>
2025-07-21 11:37:32 +0530

    fix nithish0302 removed the unused code

--- src/instructor/utils/CourseTemplata.jsx
+++ src/instructor/utils/CourseTemplata.jsx
@@ -1,39 +1,17 @@
-// const CourseTemplate = {
-//   course_name: "",
-//   level: "",
-//   language: "",
-//   total_no_hours: "",
-//   no_of_sessions: 0,
-//   no_of_tests_available: 0,
-//   tests_available: false,
-//   certification: true,
-//   pdf_available: true,
-//   price: 0,
-//   video_url: "",
-//   image_url: "",
-//   about_course: {
-//     skills: [""],
-//     complete_description: "",
-//   },
-//   outcome: [""],
-//   course_content: [],
-//   sessions: [],
-// };
-// export default CourseTemplate;
 const CourseTemplate = {
   id: null,
   course_name: "",
   level: "",
   language: "",
   total_no_hours: "",
-  manual_total_hours: false, // Flag to track if user manually set hours
+  manual_total_hours: false,
   price: 0,
   certification: false,
   pdf_available: false,
   tests_available: false,
   no_of_tests_available: 0,
-  video_file: null, // Changed from video_url to video_file
-  image_file: null, // Changed from image_url to image_file
+  video_file: null,
+  image_file: null,
   about_course: {
     skills: [],
     complete_description: "",
